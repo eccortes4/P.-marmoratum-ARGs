17ea68d9cb940ba9f926f80b0cb23fe44e9a59db
eccortes <eccortes@csu.fullerton.edu>
2026-07-24 22:24:34 +0000

    Upgrade to using parallel

--- call.sh
+++ call.sh
@@ -6,5 +6,22 @@ echo
 
 DATA="/space/s1/eccortes/frogs/working/var_sites.vcf.gz"
 
+# Using parallel for faster run times
+# If receiving error from bcftools try running bcftools index path/to/vcf/file
 #Using  niterations=0 to avoid using 4.1 phasing algo
-java -Xss5m -Xmx20g -jar ../../local/beagle.4.1.jar gl=$DATA out=~/Frog_ARGs/beagle_out/out.gt niterations=0 gprobs=true impute=false
+
+mkdir inputs ; cd inputs/
+
+parallel -a ../Scaffolds.txt "bcftools view -Oz -o inp{}.vcf.gz -r {} $DATA"
+
+parallel -j 10 "java -Xss5m -Xmx20g -jar ~/local/beagle.4.1.jar gl={1} out=~/Frog_ARGs/beagle_out/{1/.}out.gt \
+niterations=0 gprobs=true impute=false nthreads=11" ::: *.vcf.gz
+
+# Put list of new files into a file for merging
+echo
+echo "Merging files back into one"
+echo
+
+ls ~/Frog_ARGs/beagle_out | grep inp > ~/Fog_ARGS/scripts/files.txt
+bcftools merge --file-list files.txt -Oz -o called.vcf.gz
+
