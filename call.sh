a01305e228dd9c54559a199713405e36e62b83d8
eccortes <eccortes@csu.fullerton.edu>
2026-07-27 15:58:38 +0000

    Fixes to call script

--- call.sh
+++ call.sh
@@ -10,11 +10,11 @@ DATA="/space/s1/eccortes/frogs/working/var_sites.vcf.gz"
 # If receiving error from bcftools try running bcftools index path/to/vcf/file
 #Using  niterations=0 to avoid using 4.1 phasing algo
 
-mkdir inputs ; cd inputs/
+mkdir /space/s1/eccortes/frogs/temp ; cd /space/s1/eccortes/frogs/temp
 
-parallel -a ../Scaffolds.txt "bcftools view -Oz -o inp{}.vcf.gz -r {} $DATA"
+parallel -a ~/Frog_ARGs/scripts/Scaffolds.txt "bcftools view -Oz -o inp{}.vcf.gz -r {} $DATA"
 
-parallel -j 10 "java -Xss5m -Xmx20g -jar ~/local/beagle.4.1.jar gl={1} out=~/Frog_ARGs/beagle_out/{1/.}out.gt \
+parallel -j 10 "java -Xss5m -Xmx40g -jar ~/local/beagle.4.1.jar gl={1} out=/space/s1/eccortes/frogs/beagle_41_out/{1/.}out.gt \
 niterations=0 gprobs=true impute=false nthreads=11" ::: *.vcf.gz
 
 # Put list of new files into a file for merging
@@ -22,6 +22,6 @@ echo
 echo "Merging files back into one"
 echo
 
-ls ~/Frog_ARGs/beagle_out | grep inp > ~/Fog_ARGS/scripts/files.txt
-bcftools merge --file-list files.txt -Oz -o called.vcf.gz
+ls /space/s1/eccortes/frogs/beagle_41_out | grep inp > ~/Frog_ARGS/scripts/files.txt
+bcftools merge --file-list files.txt -Oz -o ~/Frog_ARGs/beagle_out/called.vcf.gz
 
