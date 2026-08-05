fca78e46fd619662096fc11abf32b0526f2089cd
eccortes <eccortes@csu.fullerton.edu>
2026-08-05 19:11:27 +0000

    Remove unnecesarry scripts

--- test_scripts/test.R
+++
@@ -1,9 +0,0 @@
-library(vcfR)
-
-vcf <- read.vcfR("~/Frog_ARGs/beagle_out/test.gt.vcf.gz")
-chroms <- getCHROM(vcf)
-
-scaffold_counts <- data.frame(table(chroms))
-colnames(scaffold_counts) <- c("Scaffold", "variable_sites")
-
-print(head(scaffold_counts))
