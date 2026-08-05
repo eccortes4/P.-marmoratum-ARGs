e340446bb10225d5ede5b50912204f05181fbb85
eccortes <eccortes@csu.fullerton.edu>
2026-08-05 20:34:18 +0000

    Remove unnecesarry scripts

--- filter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-bcftools index ~/Frog_ARGs/beagle_out/phased.gt.vcf.gz
-
-bcftools view -Oz -o ~/Frog_ARGs/beagle_out/filtered.gt.vcf.gz \
---regions-file Filtered.txt \
-~/Frog_ARGs/beagle_out/phased.gt.vcf.gz
